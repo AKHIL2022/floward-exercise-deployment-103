da72f1c8d2488bcff82317df1d6269e2711c6278
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-07-11 06:04:16 +0000

    Update Jenkinsfile (HCLCODE_Test-deployment-10)

--- ui-zip.auto.tf
+++ ui-zip.auto.tf
@@ -2,8 +2,8 @@
 # Branch: main
 locals {
   function_src_commit_url = "https://github.com/AKHIL2022/floward-exercise/"
-  function_src_commit_hash = "817722b"
-  function_src_commit_date = "2025-07-11T10:55:54+05:30"
+  function_src_commit_hash = "7302180"
+  function_src_commit_date = "2025-07-11T11:33:44+05:30"
   function_src_bucket_name = "tf-test-1"
-  function_src_object_name = "HCLCODE_Test/HCLCODE_Test-deployment-10/v20250711-053356.zip"
+  function_src_object_name = "HCLCODE_Test/HCLCODE_Test-deployment-10/v20250711-060406.zip"
 }
