9f381e5668d2ee617d719c217666bac1a9017666
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-07-15 07:48:55 +0000

    modern scm (HCLCODE_Test-deployment-10)

--- ui-zip.auto.tf
+++ ui-zip.auto.tf
@@ -4,5 +4,5 @@ locals {
   HCLCODE_Test-deployment-10_commitHash = "8c9ac96"
   HCLCODE_Test-deployment-10_commitDate = "2025-07-15T13:08:12+05:30"
   HCLCODE_Test-deployment-10_bucketName = "tf-test-1"
-  HCLCODE_Test-deployment-10_objectName = "HCLCODE_Test/HCLCODE_Test-deployment-10/v20250715-074423.zip"
+  HCLCODE_Test-deployment-10_objectName = "HCLCODE_Test/HCLCODE_Test-deployment-10/v20250715-074846.zip"
 }
