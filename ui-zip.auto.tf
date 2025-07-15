db13383bcb50c110b45dd7903060a814e70af461
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-07-15 06:00:37 +0000

    Update README.md (HCLCODE_Test-deployment-10)

--- ui-zip.auto.tf
+++ ui-zip.auto.tf
@@ -1,8 +1,8 @@
 # Lambda: HCLCODE_Test-deployment-10
 # Branch: main
 locals {
-  HCLCODE_Test-deployment-10_commitHash = "df0a686"
-  HCLCODE_Test-deployment-10_commitDate = "2025-07-15T11:27:07+05:30"
+  HCLCODE_Test-deployment-10_commitHash = "de9a87e"
+  HCLCODE_Test-deployment-10_commitDate = "2025-07-15T11:29:21+05:30"
   HCLCODE_Test-deployment-10_bucketName = "tf-test-1"
-  HCLCODE_Test-deployment-10_objectName = "HCLCODE_Test/HCLCODE_Test-deployment-10/v20250715-055827.zip"
+  HCLCODE_Test-deployment-10_objectName = "HCLCODE_Test/HCLCODE_Test-deployment-10/v20250715-060029.zip"
 }
