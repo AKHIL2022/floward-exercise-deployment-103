2bb29c68c815596f87787e1c1e3e3950943e6fc9
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-07-25 07:49:56 +0000

    Update Jenkinsfile (HCLCODE_Test-deployment-10)

--- ui-zip.auto.tf
+++ ui-zip.auto.tf
@@ -4,5 +4,5 @@ locals {
   HCLCODE_Test-deployment-10_commitHash = "7377f60"
   HCLCODE_Test-deployment-10_commitDate = "2025-07-25T13:17:48+05:30"
   HCLCODE_Test-deployment-10_bucketName = "tf-test-1"
-  HCLCODE_Test-deployment-10_objectName = "HCLCODE_Test/HCLCODE_Test-deployment-10/v20250725-074812.zip"
+  HCLCODE_Test-deployment-10_objectName = "HCLCODE_Test/HCLCODE_Test-deployment-10/v20250725-074947.zip"
 }
